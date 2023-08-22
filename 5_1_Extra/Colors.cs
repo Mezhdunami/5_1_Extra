31f7489cef7f764cf1c00569a76911d6361adfb6
Yury <taskin.jura@gmail.com>
2023-08-22 13:42:00 +0300

    Fixed MaxSpeedCoef Move enums to different .cs file

---
+++ 5_1_Extra/Colors.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_1_Extra
+{
+
+    public enum CarColor
+    {
+        Black,
+        White,
+        Green
+    }
+    internal class Colors
+    {
+    }
+}
